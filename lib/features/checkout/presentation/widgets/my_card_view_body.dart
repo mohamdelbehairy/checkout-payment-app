af4c9b5ef65454c63bd76f32d0d5cc8701db1a59
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-07-13 21:10:29 +0300

    create order info item

--- lib/features/checkout/presentation/widgets/my_card_view_body.dart
+++ lib/features/checkout/presentation/widgets/my_card_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:checkout_payment_ui/core/utils/assets.dart';
+import 'package:checkout_payment_ui/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class MyCardViewBody extends StatelessWidget {
@@ -6,10 +7,35 @@ class MyCardViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        children: [
+          const SizedBox(height: 18),
+          Image.asset(Assets.imagesBacket),
+          const SizedBox(height: 25),
+          const OrderInfoItem(title: 'Order Subtotal', value: r'$42.97'),
+          const SizedBox(height: 5),
+          const OrderInfoItem(title: 'Discount', value: r'$0'),
+          const SizedBox(height: 5),
+          const OrderInfoItem(title: 'Shipping', value: r'$8'),
+        ],
+      ),
+    );
+  }
+}
+
+class OrderInfoItem extends StatelessWidget {
+  const OrderInfoItem({super.key, required this.title, required this.value});
+  final String title, value;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
-        const SizedBox(height: 18),
-        Image.asset(Assets.imagesBacket),
+        Text(title, style: Styles.styleRegular18),
+        Text(value, style: Styles.styleRegular18)
       ],
     );
   }
