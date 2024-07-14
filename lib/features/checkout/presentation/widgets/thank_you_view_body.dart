8182091425818d9fd2c93c044aa044ca4d2fff37
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-07-14 21:08:10 +0300

    custom shaped container

--- lib/features/checkout/presentation/widgets/thank_you_view_body.dart
+++ lib/features/checkout/presentation/widgets/thank_you_view_body.dart
@@ -5,6 +5,24 @@ class ThankYouViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(32.0),
+      child: Stack(
+        children: [
+          Container(
+              decoration: BoxDecoration(
+                  color: const Color(0xffD9D9D9),
+                  borderRadius: BorderRadius.circular(20))),
+          Positioned(
+              left: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(backgroundColor: Colors.white)),
+          Positioned(
+              right: -20,
+              bottom: MediaQuery.sizeOf(context).height * .2,
+              child: const CircleAvatar(backgroundColor: Colors.white))
+        ],
+      ),
+    );
   }
 }
