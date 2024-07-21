41f52367a49aa9869272cbad18d0dcd434fd9e40
mohamdelbehairy <rahmawemohamed@gmail.com>
2024-07-21 03:06:43 +0300

    implement init payment sheet

--- lib/core/utils/strip_service.dart
+++ lib/core/utils/strip_service.dart
@@ -1,6 +1,7 @@
 import 'package:checkout_payment_ui/core/utils/api_keys.dart';
 import 'package:checkout_payment_ui/core/utils/api_service.dart';
 import 'package:checkout_payment_ui/features/checkout/data/models/payment_intent_input_model.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 import '../../features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
 
@@ -15,5 +16,13 @@ class StripService {
 
     var paymentIntentModel = PatmentIntentModel.fromJson(response.data);
     return paymentIntentModel;
+  }
+
+  Future<void> initPaymentSheet(
+      {required String paymentIntentClientSecret}) async {
+    await Stripe.instance.initPaymentSheet(
+        paymentSheetParameters: SetupPaymentSheetParameters(
+            paymentIntentClientSecret: paymentIntentClientSecret,
+            merchantDisplayName: 'Mohamed Elbehairy'));
   }
 }
